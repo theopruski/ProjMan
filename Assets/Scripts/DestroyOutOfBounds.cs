58ff9b03623efb65712ba70a0a2ad0521be5f220
theopruski <theo.pruski@gmail.com>
2024-03-27 15:49:36 +0100

    fix Map

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -4,27 +4,30 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
-    private float topBound = 30.0f;
-    private float lowerBound = -10.0f;
+    private float xMin = -1123f; // Limite inférieure sur l'axe X
+    private float xMax = -126f; // Limite supérieure sur l'axe X
+    private float yMin = 250f; // Limite inférieure sur l'axe Y
+    private float yMax = 262f; // Limite supérieure sur l'axe Y
+    private float zMin = 2401f; // Limite inférieure sur l'axe Z
+    private float zMax = 3121f; // Limite supérieure sur l'axe Z
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Position initiale : " + transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z > topBound)
-        {
-            Destroy(gameObject);
-        }
-        else if (transform.position.z < lowerBound)
+        Debug.Log("Position actuelle : " + transform.position);
+
+        if (transform.position.x < xMin || transform.position.x > xMax ||
+            transform.position.y < yMin || transform.position.y > yMax ||
+            transform.position.z < zMin || transform.position.z > zMax)
         {
             Debug.Log("Game Over!");
             Destroy(gameObject);
         }
-
     }
 }
