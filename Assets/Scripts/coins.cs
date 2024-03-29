687c29fb614c7c69a7fc5251f9c1c10e44b3e601
Nathan1Waxin <nathan.waxin@etu.unilim.fr>
2024-03-29 17:44:27 +0100

    add spawn coins(and colors)

--- Assets/Scripts/coins.cs
+++ Assets/Scripts/coins.cs
@@ -9,10 +9,12 @@ public class coins : MonoBehaviour
 {
     public float rotationSpeed = 50f;
     
-    //public GameObject coinPrefab; // Le préfab de la pièce à faire spawner
-    public int CoinNumber = 10; // Le nombre de pièces à faire spawner
-    public Vector3 spawnArea = new Vector3(10, 0, 10); // La zone dans laquelle les pièces seront spawnées
-    
+    public GameObject coinPrefab; // Le préfab de la pièce à faire spawner
+    public int CoinNumber; // Le nombre de pièces à faire spawner
+    public Vector3 spawnArea = new Vector3(1000, 0, 1000); // La zone dans laquelle les pièces seront spawnées
+    public float collisionRadius = 100f; // rayon de collision
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,12 +22,28 @@ public class coins : MonoBehaviour
         // Faire spawner des pièces aléatoirement dans la zone spécifiée
         for (int i = 0; i < CoinNumber; i++)
         {
-            Vector3 spawnPosition = new Vector3(
-                UnityEngine.Random.Range(-spawnArea.x, spawnArea.x),
-                0,
-                UnityEngine.Random.Range(-spawnArea.z, spawnArea.z)
-            );
-            //Instantiate(coinPrefab, spawnPosition, Quaternion.identity);
+            Vector3 spawnPosition = Vector3.zero;
+            bool validPosition = false;
+            while (!validPosition) {
+                spawnPosition = new Vector3(
+                UnityEngine.Random.Range(-450, -850),
+                256,
+                UnityEngine.Random.Range(2550, 2950)
+                );
+
+                Collider[] hitColliders = Physics.OverlapSphere(spawnPosition, collisionRadius);
+                validPosition = true;
+
+                foreach (Collider collider in hitColliders)
+                {
+                    if (collider.gameObject.name.StartsWith("building") && collider.gameObject.name.StartsWith("geometry"))
+                    {
+                        validPosition = false;
+                        break;
+                    }
+                }
+            }
+            Instantiate(coinPrefab, spawnPosition, Quaternion.identity);
         }
  
     }
